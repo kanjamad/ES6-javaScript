a74a475be6d3a4b1b4d3134804250ac1add3206a
kanjamad bosel <kanjamadpor@gmail.com>
2020-06-01 21:59:56 -0700

    filter example

--- filter.js
+++ filter.js
@@ -118,3 +118,56 @@ let comments = [{
         content: 'neat'
     }
 ];
+
+function commentsForPost(post, comments) {
+    return comments.filter(function (comment) {
+        return comment.postId === post.id;
+    });
+}
+
+commentsForPost(post, comments);
+
+
+
+
+// Example 4
+
+var numbers = [15, 25, 35, 45, 55, 65, 75, 85, 95];
+
+var filteredNumbers = numbers.filter(function (number) {
+    return number > 50;
+})
+
+
+filteredNumbers;
+
+
+// Example 5
+var users = [{
+        id: 1,
+        admin: true
+    },
+    {
+        id: 2,
+        admin: false
+    },
+    {
+        id: 3,
+        admin: false
+    },
+    {
+        id: 4,
+        admin: false
+    },
+    {
+        id: 5,
+        admin: true
+    },
+];
+
+var filteredUsers = users.filter(function (user) {
+    return user.admin === true;
+})
+
+
+filteredUsers;
